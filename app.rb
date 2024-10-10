b055fe972826629f5b924e4188d60b59116c676a
Neil Gevaux <neil.gevaux@york.ac.uk>
2024-10-10 15:59:20 +0100

    Partials and edit functionality

--- app.rb
+++ app.rb
@@ -1,48 +1,24 @@
 require 'sinatra'
 
-TASKS = { "list" => [] }
+TASKS = []
 
 get '/' do
   @tasks = TASKS
   erb :index
 end
 
-# New route to display tasks as a list
-get '/tasks' do
-  @tasks = TASKS
-  response.headers['Content-type'] = 'text/vnd.turbo-stream.html'
-  erb :tasks
-end
-
 post '/tasks' do
-  task_name = params[:task_name]
-  due_date = params[:due_date]
-  complete = params[:complete] == 'on'
+  @task = params['task']
+  TASKS << @task
 
-  TASKS["list"] << {
-    name: task_name,
-    due_date: due_date,
-    complete: complete
-  }
-
-  # Update the task list and send back Turbo Stream updates
-  @tasks = TASKS
   response.headers['Content-type'] = 'text/vnd.turbo-stream.html'
-  erb :tasks
+
+  erb :task_add, layout: false
 end
 
-post '/tasks/edit/:id' do
-  task_id = params[:id].to_i
-  @task = TASKS["list"][task_id]
+put '/tasks/:index' do
+  @task = TASKS[index]
+  @index = index
 
-  if @task
-    @task[:name] = params[:task_name]
-    @task[:due_date] = params[:due_date]
-    @task[:complete] = params[:complete] == 'on'
-  end
-
-  # Update the task list and send back Turbo Stream updates
-  @tasks = TASKS
-  response.headers['Content-type'] = 'text/vnd.turbo-stream.html'
-  erb :tasks
+  erb :task_update, layout:false
 end
