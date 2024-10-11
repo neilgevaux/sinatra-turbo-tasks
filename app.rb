6a789fe22464fc48f6273d221b77b6a0129be92d
Neil Gevaux <neil.gevaux@york.ac.uk>
2024-10-11 13:21:36 +0100

    Turbo functionality to add a task

--- app.rb
+++ app.rb
@@ -10,13 +10,14 @@ end
 
 post '/tasks' do
   task_id = SecureRandom.uuid
-  task = { id: task_id, name: params[:task] }
+  @task = { id: task_id, name: params[:task] }
   # @task = params['task']
-  TASKS << task
+  TASKS << @task
 
-  # response.headers['Content-type'] = 'text/vnd.turbo-stream.html'
+  response.headers['Content-type'] = 'text/vnd.turbo-stream.html'
 
-  redirect '/'
+  # redirect '/'
+  erb :tasks, layout: false
 end
 
 get '/tasks/:id/edit' do
